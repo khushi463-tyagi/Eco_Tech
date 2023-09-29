396be3ea73895341c3ede893bc2cebab53256bb8
Shark-7 <123832786+Shark-7@users.noreply.github.com>
2023-09-29 23:42:43 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\shrut\\Downloads\\flutter_windows_3.7.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\shrut\\OneDrive\\Desktop\\gfg_ecotech" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\shrut\\Downloads\\flutter_windows_3.7.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\shrut\\OneDrive\\Desktop\\gfg_ecotech"
+  "FLUTTER_ROOT=C:\\Users\\shrut\\Downloads\\flutter_windows_3.7.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\shrut\\OneDrive\\Desktop\\gfg_ecotech\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\shrut\\OneDrive\\Desktop\\gfg_ecotech"
+  "FLUTTER_TARGET=C:\\Users\\shrut\\OneDrive\\Desktop\\gfg_ecotech\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\shrut\\OneDrive\\Desktop\\gfg_ecotech\\.dart_tool\\package_config.json"
+)
